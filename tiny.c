30af021090bc5be2885b56521d9176d2dfb68fab
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2026-01-27 01:48:41 +0000

    Refactor tiny.c to use run function and update syscalls

--- tiny.c
+++ tiny.c
@@ -1,8 +1,11 @@
 #define SYS_READ  0
 #define SYS_WRITE 1
 #define SYS_OPEN  2
-#define SYS_POLL  7
-#define SYS_EXIT  60
+#define SYS_LSEEK 8
+
+int balance = 100;
+int fd;
+char buf;
 
 static inline long sc(long n, long a1, long a2, long a3) {
     long r;
@@ -10,28 +13,28 @@ static inline long sc(long n, long a1, long a2, long a3) {
     return r;
 }
 
-void __attribute__((noreturn)) _start() {
-    long balance = 100;
-    long fd = sc(SYS_OPEN, (long)"/dev/shm/g", 66, 0666);
+void run() {
+    fd = sc(SYS_OPEN, (long)"/dev/shm/g", 66, 0666);
     sc(SYS_READ, fd, (long)&balance, 4);
+    if (balance <= 0) balance = 100;
 
     while (balance > 0) {
-        unsigned long target, count = 1;
-        
-        __asm__ volatile ("rdrand %0" : "=r"(target));
-        target = (target % 12) + 1;
+        unsigned long start_tsc, end_tsc;
 
         sc(SYS_WRITE, 1, (long)"$ ", 2);
 
-        struct { int fd; short ev, rev; } p = {0, 1, 0};
-        while (sc(SYS_POLL, (long)&p, 1, 0) <= 0) {
-            count = (count % 12) + 1;
+        __asm__ volatile ("rdtsc" : "=a"(start_tsc) : : "edx");
+
+        sc(SYS_READ, 0, (long)&buf, 1);
+        
+        if (buf != '\n') {
+            char junk;
+            while (sc(SYS_READ, 0, (long)&junk, 1) > 0 && junk != '\n');
         }
 
-        char buf;
-        sc(SYS_READ, 0, (long)&buf, 1);
+        __asm__ volatile ("rdtsc" : "=a"(end_tsc) : : "edx");
 
-        if (count == target) {
+        if ((end_tsc - start_tsc) % 10 > 7) { 
             balance += 50;
             sc(SYS_WRITE, 1, (long)"W\n", 2);
         } else {
@@ -39,10 +42,12 @@ void __attribute__((noreturn)) _start() {
             sc(SYS_WRITE, 1, (long)"L\n", 2);
         }
 
-        sc(8, fd, 0, 0); 
+        sc(SYS_LSEEK, fd, 0, 0);
         sc(SYS_WRITE, fd, (long)&balance, 4);
     }
+    sc(60, 0, 0, 0);
+}
 
-    sc(SYS_EXIT, 0, 0, 0);
-    __builtin_unreachable();
+void __attribute__((naked)) _start() {
+    __asm__ volatile ("call run\n");
 }
