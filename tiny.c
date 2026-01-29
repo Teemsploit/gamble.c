0cbae68d4ea627982f12e6431a811654ced6ff01
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2026-01-29 12:33:15 +0000

    Refactor syscall handling and main function structure

--- tiny.c
+++ tiny.c
@@ -1,53 +1,50 @@
-#define SYS_READ  0
-#define SYS_WRITE 1
-#define SYS_OPEN  2
-#define SYS_LSEEK 8
+#define SYS_EXIT  1
+#define SYS_READ  3
+#define SYS_WRITE 4
+#define SYS_OPEN  5
+#define SYS_LSEEK 19
 
-int balance = 100;
-int fd;
-char buf;
-
-static inline long sc(long n, long a1, long a2, long a3) {
-    long r;
-    __asm__ volatile ("syscall" : "=a"(r) : "a"(n), "D"(a1), "S"(a2), "d"(a3) : "rcx", "r11", "memory");
+static inline int sc(int n, int a1, int a2, int a3) {
+    int r;
+    __asm__ volatile ("int $0x80" : "=a"(r) : "a"(n), "b"(a1), "c"(a2), "d"(a3) : "memory");
     return r;
 }
 
-void run() {
-    fd = sc(SYS_OPEN, (long)"/dev/shm/g", 66, 0666);
-    sc(SYS_READ, fd, (long)&balance, 4);
-    if (balance <= 0) balance = 100;
+void _start() {
+    int balance;
+    int fd;
+    char buf;
+
+    fd = sc(SYS_OPEN, (int)"/dev/shm/g", 66, 0666);
+    
+    if (sc(SYS_READ, fd, (int)&balance, 4) <= 0) {
+        balance = 100;
+    }
 
     while (balance > 0) {
-        unsigned long start_tsc, end_tsc;
+        unsigned int t1, t2;
 
-        sc(SYS_WRITE, 1, (long)"$ ", 2);
-
-        __asm__ volatile ("rdtsc" : "=a"(start_tsc) : : "edx");
-
-        sc(SYS_READ, 0, (long)&buf, 1);
+        sc(SYS_WRITE, 1, (int)"$ ", 2);
+        
+        __asm__ volatile ("rdtsc" : "=a"(t1) : : "edx");
+        sc(SYS_READ, 0, (int)&buf, 1);
         
         if (buf != '\n') {
             char junk;
-            while (sc(SYS_READ, 0, (long)&junk, 1) > 0 && junk != '\n');
+            while (sc(SYS_READ, 0, (int)&junk, 1) > 0 && junk != '\n');
         }
+        __asm__ volatile ("rdtsc" : "=a"(t2) : : "edx");
 
-        __asm__ volatile ("rdtsc" : "=a"(end_tsc) : : "edx");
-
-        if ((end_tsc - start_tsc) % 10 > 7) { 
+        if ((t2 - t1) % 10 > 7) {
             balance += 50;
-            sc(SYS_WRITE, 1, (long)"W\n", 2);
+            sc(SYS_WRITE, 1, (int)"W\n", 2);
         } else {
             balance -= 10;
-            sc(SYS_WRITE, 1, (long)"L\n", 2);
+            sc(SYS_WRITE, 1, (int)"L\n", 2);
         }
 
         sc(SYS_LSEEK, fd, 0, 0);
-        sc(SYS_WRITE, fd, (long)&balance, 4);
+        sc(SYS_WRITE, fd, (int)&balance, 4);
     }
-    sc(60, 0, 0, 0);
-}
-
-void __attribute__((naked)) _start() {
-    __asm__ volatile ("call run\n");
+    sc(SYS_EXIT, 0, 0, 0);
 }
